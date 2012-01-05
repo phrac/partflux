69592a7c058ba20c315c971e571b1af8fe4a71fe
Derek Musselmann <derek@disflux.com>
2012-01-04 19:46:02 -0600

    minor formatting

--- urls.py
+++ urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'main.views.home'),
-	url(r'^parts/$', 'parts.views.index'),
-	url(r'^parts/(?P<part_id>\d+)/$', 'parts.views.detail'),
-	url(r'^parts/search/$', 'parts.views.search'),
+    url(r'^parts/$', 'parts.views.index'),
+    url(r'^parts/(?P<part_id>\d+)/$', 'parts.views.detail'),
+    url(r'^parts/search/$', 'parts.views.search'),
 
-    # Uncomment the next line to enable the admin:
+    # activate admin stuff
     url(r'^admin/', include(admin.site.urls)),
 )
