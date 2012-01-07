55e68f2cd306db77bbf805e4237d281452a653ea
Derek Musselmann <derek@disflux.com>
2012-01-07 02:07:43 -0600

    very minor visual code formatting

--- parts/views.py
+++ parts/views.py
@@ -50,28 +50,19 @@ def detail(request, part_id):
 							  	context_instance=RequestContext(request))
 
 def search(request):
-	q = request.GET.get('q', '')
-	if q:
-		#partresults = Part.ft.filter(number__istartswith=q).defer("metadata", "tsv")[:25]
-		#ftresults = Part.ft.search(q).defer("metadata", "tsv")[:25]
-		
-		results = Part.objects.filter(
-                                        Q(number__istartswith=q) |
-								        Q(tsv__query=q)
-							        )[:25]
-		
-		numresults = Part.objects.filter(
-							            Q(number__istartswith=q) |
-            							Q(tsv__query=q)
-			            				).count()
-	else:
-		partresults = []
-	return render_to_response('parts/index.html',
-								{ 
-									'parts_list': results, 
-									'searchterm': q,
-									'numresults': numresults
-								},
-								context_instance=RequestContext(request))
+    q = request.GET.get('q', '')
+    if q:
+        results = Part.objects.filter(Q(number__istartswith=q) | Q(tsv__query=q))[:25]
+        numresults = Part.objects.filter(Q(number__istartswith=q) | Q(tsv__query=q)).count()
+    else:
+        partresults = []
+    
+    return render_to_response('parts/index.html',
+                              { 
+                                  'parts_list': results, 
+                                  'searchterm': q,
+                                  'numresults': numresults
+                              },
+                              context_instance=RequestContext(request))
 
 
