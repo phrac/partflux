f44f37249e7cc8793d5a2068c8715e640b1de501
Derek Musselmann <derek@disflux.com>
2012-01-08 12:28:24 -0600

    setup companies in views and templates

--- companies/views.py
+++ companies/views.py
@@ -1,1 +1,31 @@
-# Create your views here.
+from django.shortcuts import render_to_response, redirect, get_object_or_404
+from django.template import RequestContext
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+
+from companies.models import Company
+
+def index(request):
+    companies_list = Part.objects.all().order_by('created_at')
+    paginator = Paginator(parts_list, 20)
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        companies = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        companies = paginator.page(paginator.num_pages)
+
+    return render_to_response('companies/index.html', 
+                              {'companies_list': companies}, 
+                              context_instance=RequestContext(request))
+
+def detail(request, company_id):
+     c = get_object_or_404(Part, pk=part_id)
+     
+     return render_to_response('parts/detail.html',
+                                {'company': c,}
+                                context_instance=RequestContext(request))
+
--- urls.py
+++ urls.py
@@ -10,6 +10,9 @@ urlpatterns = patterns('',
     url(r'^parts/(?P<part_id>\d+)/$', 'parts.views.detail'),
     url(r'^parts/search/$', 'parts.views.search'),
 
+    url(r'^companies/$', 'companies.views.index'),
+    url(r'^companies/(?P<company_id>\d+)/$', 'companies.views.detail'),
+
     (r'^users/', include('registration.backends.default.urls')),
 
     # activate admin stuff
