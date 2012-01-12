66a2933c7066c8534646da7607ad7395b69efe3f
Derek Musselmann <derek@disflux.com>
2012-01-12 12:06:58 -0600

    wsgi script added

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # Django settings for partfindr project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -20,7 +20,7 @@ DATABASES = {
         'NAME': 'partfindr',                     	# Or path to database file if using sqlite3.
         'USER': 'derek',                      				# Not used with sqlite3.
         'PASSWORD': '',                  			# Not used with sqlite3.
-        'HOST': 'prime.disflux.org',                      	# Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      	# Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      					# Set to empty string for default. Not used with sqlite3.
     }
 }
@@ -50,18 +50,18 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/derek/web/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/derek/web/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,14 +70,14 @@ STATIC_URL = '/static/'
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = 'http://partfindr.net/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	"/home/derek/web/partfindr/static",
+	"/home/derek/partfindr/static",
 )
 
 # List of finder classes that know how to find static files in
@@ -122,7 +122,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	"/home/derek/web/partfindr/templates"
+	"/home/derek/partfindr/templates"
 )
 
 INSTALLED_APPS = (
