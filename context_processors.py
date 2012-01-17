eb0cf3505d9bc2ab222213e2329b4ed316793be2
Derek Musselmann <derek@disflux.com>
2012-01-17 17:03:12 -0600

    add a users app. adjustments to login pages and where they redirect to

--- context_processors.py
+++ context_processors.py
@@ -1,5 +1,12 @@
 def part_count(request):
 	from parts.models import Part
-	return{'part_count': Part.objects.count()}
+	return {
+        'part_count': Part.objects.count()
+    }
+
+def get_current_path(request):
+    return {
+        'current_path': request.get_full_path()
+    }
 
 
