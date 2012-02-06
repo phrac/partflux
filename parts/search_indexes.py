87a0b59a0d76a308da335f64a8c77f3d0c2cb4cf
Derek Musselmann <derek@disflux.com>
2012-02-06 17:50:15 -0600

    faceting on company and metadata for solr

--- parts/search_indexes.py
+++ parts/search_indexes.py
@@ -5,7 +5,8 @@ from parts.models import Part
 class PartIndex(indexes.SearchIndex, indexes.Indexable):
 	text = indexes.CharField(document=True, use_template=True)
 	number = indexes.NgramField(model_attr='number', boost=3)
-	company = indexes.CharField(model_attr='company')
+	company = indexes.CharField(model_attr='company', faceted=True)
+    metadata = indexes.CharField(model_attr='metadata', faceted=True)
 
 	def get_model(self):
 		return Part
