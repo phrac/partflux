85c8aecbda5687895869812ae3788b8fe42a0284
Derek Musselmann <derek@disflux.com>
2012-02-07 12:58:07 -0600

    search improvements

--- search/views.py
+++ search/views.py
@@ -37,12 +37,21 @@ def results(request):
     
     if searchform.is_valid():
         q = searchform.cleaned_data['q']
-        if q:
+        selected_facets = request.GET.getlist("selected_facets")
+		if q:
             #results = Part.objects.filter(Q(number__istartswith=q) |
             #                             Q(tsv__query=q)).distinct().only('number', 'description', 'company')
-            sqs = SearchQuerySet()
+            sqs = SearchQuerySet().facet('company')
 
-            results = sqs.filter(content=AutoQuery(q)).facet('company')
+            results = sqs.filter(content=AutoQuery(q))
+
+			for facet in selected_facets:
+				if ":" not in facet:
+					continue
+				field, value = facet.split(":", 1)
+
+				if value:
+					results = results.narrow(u'%s:"%s"' % (field, sqs.query.clean(value)))
         else:
             results = []
     
@@ -58,5 +67,6 @@ def results(request):
                               { 
                                   'parts_list': parts_list, 
                                   'searchterm': q,
+								  'facets': results.facet_counts()
                               },
                               context_instance=RequestContext(request))
