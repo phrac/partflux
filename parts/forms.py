6aa1258de09ca6d764944cb13b05dd88d3641fda
Derek Musselmann <derek@disflux.com>
2012-02-09 12:28:01 -0600

    when adding a part, longer length of description should be allowed.

--- parts/forms.py
+++ parts/forms.py
@@ -3,12 +3,12 @@ from parts.models import Part
 
 class MetadataForm(forms.Form):
     key = forms.CharField(max_length=48)
-    value = forms.CharField(max_length=96)
+    value = forms.CharField(max_length=128)
 
 class XrefForm(forms.Form):
     part = forms.CharField(max_length=48)
     company = forms.CharField(max_length=48)
-    desc = forms.CharField(max_length=48, required=False)
+    desc = forms.CharField(max_length=256, required=False)
 
 class SearchForm(forms.Form):
     q = forms.CharField(max_length=128)
