97db096dfef218e04e8edc6c0a97175794da9b9d
Derek Musselmann <derek@disflux.com>
2012-05-14 18:49:59 -0500

    remove some stuff for smaller indexes

--- nsn/search_indexes.py
+++ nsn/search_indexes.py
@@ -3,9 +3,7 @@ from nsn.models import Nsn
 
 class NsnIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    fsc = indexes.CharField(model_attr='fsc')
-    lastupdate = indexes.DateTimeField(model_attr='updated_at')
-
+    
     def get_model(self):
         return Nsn
 
--- parts/search_indexes.py
+++ parts/search_indexes.py
@@ -1,12 +1,10 @@
 from haystack import indexes
 from parts.models import Part
 
-
 class PartIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     number = indexes.NgramField(model_attr='number', boost=3)
     company = indexes.CharField(model_attr='company', faceted=True)
-    lastupdate = indexes.DateTimeField(model_attr='updated_at')
 
     def get_model(self):
         return Part
