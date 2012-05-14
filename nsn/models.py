fd71ddbca35ed231f1987271978c784017cf6462
Derek Musselmann <derek@disflux.com>
2012-05-14 14:47:43 -0500

    add an updated_at field to NSN

--- nsn/models.py
+++ nsn/models.py
@@ -4,6 +4,7 @@ class Nsn(models.Model):
     number = models.CharField(max_length=16, unique=True)
     fsc = models.ForeignKey('Fsc', null=True)
     description = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
      
     def __unicode__(self):
         return self.number
