4bf12e3131893248d1863be88ada8c4ac28cbef6
Derek Musselmann <derek@disflux.com>
2012-06-17 08:49:34 -0500

    extend the User model with basic extra information

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    reputation = models.IntegerField()
+    facebook_profile = models.URLField()
+    twitter_profile = models.URLField()
+    linkedin_profile = models.URLField()
+
+
