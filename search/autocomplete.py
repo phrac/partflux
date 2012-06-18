687942c1c4d0988548966680d0ce75b261cd32be
Derek Musselmann <derek@disflux.com>
2012-06-18 17:25:20 -0500

    fix #6 by checking if request is ajax

--- search/autocomplete.py
+++ search/autocomplete.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.template.defaultfilters import truncatechars
 from django.conf import settings
 
@@ -10,26 +10,36 @@ def autocomplete(request):
     Handles jQuery XHR requests for autocomplete 
     
     """
-    term = request.GET['term']
-    type = request.GET['type']
-    conn = ES(settings.ES_HOST)
-    
-    if type == 'company':
-        query = PrefixQuery(field='company_name', prefix=term )
-    if type == 'part':
-        query = PrefixQuery(field='number',prefix=term)
+    if request.is_ajax():
+        if not "term" in request.GET:
+            return HttpResponseNotFound()
+        else:
+            term = request.GET['term']
+        if not "type" in request.GET:
+            return HttpResponseNotFound()
+        else:
+            type = request.GET['type']
 
-    s = Search(query, fields=['company_name', 'number', 'desc'], size=10)
-    raw_results = conn.search(s)
-        
-    results = []
-    for r in raw_results:
+        conn = ES(settings.ES_HOST)
+
         if type == 'company':
-            results.append(r.company_name)
+            query = PrefixQuery(field='company_name', prefix=term )
         if type == 'part':
-            results.append({'label': "%s - %s" % (r.number,
-                                                  truncatechars(r.desc,60)),
-                                                  'value': r.number})
+            query = PrefixQuery(field='number',prefix=term)
 
-    return HttpResponse(json.dumps(results), mimetype="application/json")
+        s = Search(query, fields=['company_name', 'number', 'desc'], size=10)
+        raw_results = conn.search(s)
+
+        results = []
+        for r in raw_results:
+            if type == 'company':
+                results.append(r.company_name)
+            if type == 'part':
+                results.append({'label': "%s - %s" % (r.number,
+                                                      truncatechars(r.desc,60)),
+                                'value': r.number})
+
+        return HttpResponse(json.dumps(results), mimetype="application/json")
+    else:
+        return HttpResponseNotFound('ajax only')
 
