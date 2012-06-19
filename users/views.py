53c40ff75281a5c934a3529407dba2f46d7f62bc
Derek Musselmann <derek@disflux.com>
2012-06-18 22:33:09 -0500

    initial profile view and additions to UserProfile model

--- users/views.py
+++ users/views.py
@@ -1,4 +1,16 @@
 from users.models import UserProfile
+from django.contrib.auth import User
+from django.shortcuts import render_to_response, redirect, get_object_or_404
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
 
-def view_profile(request):
-    pass
+@login_required
+def view_profile(request, username):
+    user = get_object_or_404(User, username=username)
+
+    return render_to_response('users/profile.html',
+                              {'user_profile' : profile,
+                              },
+                              context_instance=RequestContext(request))
+
+
