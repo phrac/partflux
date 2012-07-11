04bd6c717c68926aede4cb08a21c4ce5057f1ad5
Derek Musselmann <derek@disflux.com>
2012-07-11 08:10:13 -0500

    add sitemaps framework

--- sitemap.py
+++ sitemap.py
@@ -1,9 +1,29 @@
-import datetime
+from django.shortcuts import render
+from django.template import RequestContext
+from datetime import date, timedelta, datetime
 
-START_DATE = '20120101'
+from parts.models import Part
 
 def index(request):
-    pass
+    START_DATE = date(2012, 1, 1)
+    END_DATE = date.today()
+    td = datetime.timedelta(hours=24)
+    current_date = END_DATE
+    datelist = []
+    while current_date >= START_DATE:
+        datelist.append(current_date)
+        current_date -= td
+
+    return render(request, 'sitemaps/index.xml',
+                  { 'dates': datelist },
+                  content_type='application/xml')
     
-def sitemap(request, date):
-    pass
+
+def sitemap(request, sitemap_date):
+    date_object = datetime.strptime(sitemap_date, '%Y-%m-%d')
+    next_date = date_object + timedelta(hours=24)
+    parts = Part.objects.filter(created_at__range=(date_object, next_date))
+
+    return render(request, 'sitemaps/sitemap.xml',
+                  { 'parts' : parts },
+                  content_type='application/xml')
