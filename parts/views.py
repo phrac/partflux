284a585197e4608bbd5dc3e90aadf36165d68b95
Derek Musselmann <derek@disflux.com>
2012-11-10 12:16:08 -0600

    finalize new layout for part details

--- parts/views.py
+++ parts/views.py
@@ -17,7 +17,7 @@ from users.models import UserProfile
 import json
 
 def index(request):
-    parts_list = Part.objects.all().order_by('-created_at')[:25]
+    parts_list = Part.objects.all().order_by('-created_at')[:100]
 
     return render_to_response('parts/index.html',
                               {'parts_list': parts_list},
