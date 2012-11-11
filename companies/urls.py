f3b4683aafbc9ae64c6321625b5f4ac364a5b4f2
Derek Musselmann <derek@disflux.com>
2012-11-11 11:28:54 -0600

    updates to company detail page; add bootstrap-editable for editing things in the future

--- companies/urls.py
+++ companies/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'companies.views.index'),
-    url(r'^edit/(?P<company_slug>.+)/$', 'companies.views.edit'),
+    url(r'^edit/(?P<company_id>\d+)/(?P<company_slug>.+)/$', 'companies.views.edit'),
     url(r'^(?P<company_id>\d+)/(?P<company_slug>.+)/$', 'companies.views.detail'),
     url(r'^(?P<company_slug>.+)/$', 'companies.views.redirect_new_page'),
 )
