2d70a671ab91d6cc7e234d522aa7e703d4ef288f
Derek Musselmann <derek@disflux.com>
2012-11-12 16:27:46 -0600

    add a preview site uwsgi settings file

---
+++ partengine-preview_wsgi.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+sys.path.append('/home/derek')
+sys.path.append('/home/derek/partengine-preview')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'partfindr.settings'
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
