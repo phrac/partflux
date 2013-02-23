e853bb21a1af6bf50b7ae6c7b2b00de2a2ee59d5
Derek Musselmann <derek@disflux.com>
2013-02-23 09:05:30 -0600

    check if autocomplete is coming via ajax, raise Http404 if its not

--- search/autocomplete.py
+++ search/autocomplete.py
@@ -3,13 +3,19 @@ from django.template.defaultfilters import truncatechars
 from django.conf import settings
 from haystack.query import SearchQuerySet
 from haystack.inputs import AutoQuery
+from django.http import Http404
 
 import json
 
 def autocomplete(request):
-    q = request.GET.get('q', '')
-    sqs = SearchQuerySet().filter(content__startswith=q)[:5]
-    suggestions = [result.object.number for result in sqs]
-    the_data = json.dumps(suggestions)
-    return HttpResponse(the_data, content_type='application/json')
+    if request.is_ajax():
+        q = request.GET.get('q', '')
+        sqs = SearchQuerySet().filter(content__startswith=q)[:5]
+        suggestions = [result.object.number for result in sqs]
+        the_data = json.dumps(suggestions)
+        return HttpResponse(the_data, content_type='application/json')
+
+    else:
+        raise Http404
+
 
