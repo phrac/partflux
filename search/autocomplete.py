361b5fc63e40d8c43adb166dc6063f5ea311a7ef
Derek Musselmann <derek@disflux.com>
2013-02-22 23:42:52 -0600

    autocomplete now working on partial number match

--- search/autocomplete.py
+++ search/autocomplete.py
@@ -8,13 +8,8 @@ import json
 
 def autocomplete(request):
     q = request.GET.get('q', '')
-    sqs = SearchQuerySet().autocomplete(q)[:25]
-    print q
-    print sqs
-    #results = sqs.filter(number__startswith=q)[:5]
+    sqs = SearchQuerySet().filter(content__startswith=q)[:5]
     suggestions = [result.object.number for result in sqs]
-    # Make sure you return a JSON object, not a bare list.
-    # Otherwise, you could be vulnerable to an XSS attack.
     the_data = json.dumps(suggestions)
     return HttpResponse(the_data, content_type='application/json')
 
