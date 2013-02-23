822b8ac2b2bdc3dfd04210c818f24fd774fdc03f
Derek Musselmann <derek@disflux.com>
2013-02-22 23:16:57 -0600

    accidentally removed name from search form causing no results

--- search/autocomplete.py
+++ search/autocomplete.py
@@ -2,12 +2,17 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.template.defaultfilters import truncatechars
 from django.conf import settings
 from haystack.query import SearchQuerySet
+from haystack.inputs import AutoQuery
 
 import json
 
 def autocomplete(request):
-    sqs = SearchQuerySet().filter(content_auto=request.GET.get('q', ''))[:5]
-    suggestions = ['one', 'two', 'three',]
+    q = request.GET.get('q', '')
+    sqs = SearchQuerySet().filter(number__contains=q)[:25]
+    print q
+    print sqs
+    #results = sqs.filter(number__startswith=q)[:5]
+    suggestions = [result.object.number for result in sqs]
     # Make sure you return a JSON object, not a bare list.
     # Otherwise, you could be vulnerable to an XSS attack.
     the_data = json.dumps(suggestions)
