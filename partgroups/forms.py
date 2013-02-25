effbb555bf4f485d62681b8195093312738161bf
Derek Musselmann <derek@disflux.com>
2013-02-25 09:18:55 -0600

    make templates pass validation tests

--- partgroups/forms.py
+++ partgroups/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from django.forms import ModelForm
-from parts.models import Part
+from partgroups.models import PartGroup
+
 
 class PartGroupForm(ModelForm):
     name = forms.CharField(max_length=32)
     description = forms.CharField(required=True, widget=forms.Textarea)
     private = forms.BooleanField(required=False)
+    
+    class Meta:
+        model = PartGroup
 
 
 
