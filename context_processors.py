2e1ce29ed5345b3847bdeb55ec11fce55e2a4e7e
Derek Musselmann <derek@disflux.com>
2013-02-28 16:37:06 -0600

    make sure a userprofile exists; show that a user is signed in on the landing page

--- context_processors.py
+++ context_processors.py
@@ -38,8 +38,12 @@ def get_current_domain(request):
 
 def user_reputation(request):
     from django.contrib.auth.models import User
+    from users.models import UserProfile
     if request.user.is_authenticated():
-        profile = request.user.get_profile()
+        try:
+            profile = request.user.get_profile()
+        except:
+            profile = UserProfile.objects.create(user=request.user)
         return { 'user_reputation': profile.reputation }
     else:
         return { 'user_reputation': None }
