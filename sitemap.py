0f198a333c423aeda238bcd29346b1ed09612d71
Derek Musselmann <derek@disflux.com>
2013-03-07 11:56:25 -0600

    more tweaks to sitemaps

--- sitemap.py
+++ sitemap.py
@@ -9,6 +9,8 @@ from parts.models import Part
 from companies.models import Company
 from nsn.models import Nsn
 
+LINKS_PER_PAGE = 1000
+
 def index(request):
     START_DATE = date(2012, 1, 1)
     END_DATE = date.today() - timedelta(hours=24)
@@ -22,6 +24,37 @@ def index(request):
     return render(request, 'sitemaps/index.xml',
                   { 'dates': datelist },
                   content_type='application/xml')
+    
+def object_index(request, sitemap_type, sitemap_date):
+    current_date = datetime.strptime(sitemap_date, '%Y-%m-%d')
+    next_date = current_date + timedelta(hours=24)
+    if sitemap_type == 'parts':
+        obj = Part.objects.filter(created_at__range=(current_date, next_date))
+    elif sitemap_type == 'companies':
+        obj = Company.objects.filter(created_at__range=(current_date, next_date))
+    elif sitemap_type == 'nsn':
+        obj = Nsn.objects.filter(updated_at__range=(current_date, next_date))
+        
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    p = Paginator(obj, LINKS_PER_PAGE, request=request)
+    try:
+        obj_list = p.page(page)
+    except (PageNotAnInteger, EmptyPage):
+        obj_list = p.page(1)
+        
+    results_list = []
+    print p.num_pages
+    for page in range(1, p.num_pages + 1):
+        results_list.append("http://partengine.org/sitemap-%s-index-%s.xml?page=%s" % (sitemap_type, sitemap_date, page))    
+    
+    return render(request, 'sitemaps/object_index.xml',
+                 {'objects_list': results_list,
+                  'current_domain': 'http://partengine.org',
+                 }, content_type='application/xml')
     
 
 def sitemap(request, sitemap_type, sitemap_date):
@@ -39,7 +72,7 @@ def sitemap(request, sitemap_type, sitemap_date):
     except ValueError:
         page = 1
 
-    p = Paginator(obj, 500, request=request)
+    p = Paginator(obj, LINKS_PER_PAGE, request=request)
     try:
         obj_list = p.page(page)
     except (PageNotAnInteger, EmptyPage):
