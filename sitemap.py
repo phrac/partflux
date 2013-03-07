35b11c15171d1c38c907413138d213e0ad168a71
Derek Musselmann <derek@disflux.com>
2013-03-07 09:31:33 -0600

    clean up the way sitemaps are handled

--- sitemap.py
+++ sitemap.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from django.template import RequestContext
 from datetime import date, timedelta, datetime
 from django.template import loader, Context
+from pure_pagination import Paginator, PageNotAnInteger, EmptyPage
 
 from parts.models import Part
 from companies.models import Company
+from nsn.models import Nsn
 
 def index(request):
     START_DATE = date(2012, 1, 1)
@@ -22,15 +24,28 @@ def index(request):
                   content_type='application/xml')
     
 
-def sitemap(sitemap_date):
+def sitemap(request, sitemap_type, sitemap_date):
     current_date = datetime.strptime(sitemap_date, '%Y-%m-%d')
     next_date = current_date + timedelta(hours=24)
-    parts = Part.objects.filter(created_at__range=(current_date, next_date))[:50000]
-    filename = "/home/derek/web/static/sitemaps/sitemap-parts-%s.xml" % sitemap_date
-    open(filename, "w").write(render_to_string('sitemaps/sitemap.xml', {'objects': parts, 'current_domain' : 'http://partengine.org'}))
-    companies = Company.objects.filter(created_at__range=(current_date, next_date))
-    print "wrote %s" % filename
-    filename = "/home/derek/web/static/sitemaps/sitemap-companies-%s.xml" % sitemap_date
-    open(filename, "w").write(render_to_string('sitemaps/sitemap.xml', {'objects': companies, 'current_domain' : 'http://partengine.org'}))
-    print "wrote %s" % filename
+    if sitemap_type == 'parts':
+        obj = Part.objects.filter(created_at__range=(current_date, next_date))
+    elif sitemap_type == 'companies':
+        obj = Company.objects.filter(created_at__range=(current_date, next_date))
+    elif sitemap_type == 'nsn':
+        obj = Nsn.objects.filter(updated_at__range=(current_date, next_date))
+        
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    p = Paginator(obj, 500, request=request)
+    try:
+        obj_list = p.page(page)
+    except (PageNotAnInteger, EmptyPage):
+        obj_list = p.page(1)    
+    return render(request, 'sitemaps/sitemap.xml',
+                 {'objects_list': obj_list,
+                  'current_domain': 'http://partengine.org',
+                 }, content_type='application/xml')
 
