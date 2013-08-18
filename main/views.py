ee7f4aa33ae5f1558011c798d5f8144c8c31dd66
Derek Musselmann <derek@disflux.com>
2013-08-18 10:41:21 -0500

    more template updates and fixes

--- main/views.py
+++ main/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from parts.models import Part
+from distributors.models import DistributorSKU
 
 def index(request):
-    return render_to_response('main/comingsoon.html',
+    part_updates = Part.objects.all().order_by('-updated_at')[:7]
+    dist_updates = DistributorSKU.objects.all().order_by('-updated')[:7]
+    return render_to_response('main/index.html',
                               {
+                                  'parts': part_updates,
+                                  'skus': dist_updates,
                               },
                               context_instance=RequestContext(request))
 
