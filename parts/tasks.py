69c1214369bc130b046501b68806e5bb436a35f2
Derek Musselmann <derek@disflux.com>
2014-04-29 12:05:16 -0500

    fix the recursion task to prevent maximum stack depth errors

--- parts/tasks.py
+++ parts/tasks.py
@@ -1,9 +1,30 @@
 from celery import task
+from parts.models import Part
 
 @task()
 def update_all_xrefs(part_id):
-    from parts.models import Part
+    part = Part.objects.get(id=part_id)
+    xrefs = get_xrefs(part_id)
+    for x in xrefs:
+        for p in xrefs:
+            x.cross_references.add(p)
+
+def get_xrefs(part_id):
+    xrefs = []
+    sub = []
     part = Part.objects.get(id=part_id)
     for p in part.cross_references.all():
-        p.cross_references.add(part)
-        update_all_xrefs(p.id)
+        xrefs.append(p)
+        if p in xrefs:
+            pass
+        else:
+            sub = get_xrefs(p.id)
+    for s in sub:
+        xrefs.append(s)
+
+    return xrefs
+
+
+
+
+
